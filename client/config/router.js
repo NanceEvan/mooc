56bf709b6bf996855dedd25396a6e325db0d4bf4
sizer <591207060@qq.com>
2019-05-03 20:24:40 +0800

    scrollBehavior

--- client/config/router.js
+++ client/config/router.js
@@ -18,6 +18,19 @@ export default () => {
         // linkActive应该是linkExactActive的子集
         // linkActiveClass: 'active-link',
         // linkExactActiveClass: 'exact-active-link',
+
+        scrollBehavior (to, from, savedPosition) {
+            // 使用前端路由， 当切换到新路由时， 想要页面滚到顶部， 或者保持原先的关东位置， 就像重新加载页面那样
+            // 这个功能只在HTML5 history 模式下可用
+            // scrollBehavior方法接收to 和 from 路由对象， 第三个参数 savedPosition当且仅当popstate导航（通过浏览器的前进/后退按钮触发时才可以用）
+            // 这个方法返回滚动信息的对象信息， just like: {x: number, y: number} {selector: string, offset?:{x: number, y:number}}
+            // 如果返回一个falsy（不是false）的值， 或者返回一个空对象， 就不会发生滚动
+            if (savedPosition) {
+                return savedPosition
+            } else {
+                return {x: 0, y: 0}
+            }
+        },
         routes,
     })
 }
